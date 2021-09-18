569d47a063a9437fb03a78b0ff7d9fdf3c7b5bfa
jake <beej8046@gmail.com>
2021-09-17 17:17:03 -0800

    RPS start

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let rpsHashy = {
     "scissors": ["paper", "rock"]
 };
 let keys = Object.keys(rpsHashy);
-let playaPlay = "scissors";
+let playaPlay = window.prompt("Pick one: rock, paper, scissors");
 const computuhPlay = keys[Math.floor((Math.random() * 3))];
 
-function play(computuhPlay, playaPlay){
-    if (computuhPlay === playaPlay) 
-         return console.log("It's a tie!");
+function play(computuhPlay, playaPlay) {
+    if (computuhPlay === playaPlay)
+        return console.log("It's a tie!");
     else if (computuhPlay === rpsHashy[playaPlay][0])
         return console.log("You win!");
     else
