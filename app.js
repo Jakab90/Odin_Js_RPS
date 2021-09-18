cae140f290a0a20da6e6b48c7f1bc64cd526c407
jake <beej8046@gmail.com>
2021-09-18 14:47:48 -0800

    Added GUI elements

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
+let playaScore = 0;
+let computuhScore = 0;
+const buttons = document.querySelectorAll('input');
 let rpsHashy = {
     "rock": ["scissors", "paper"],
     "paper": ["rock", "scissors"],
     "scissors": ["paper", "rock"]
 };
-let keys = Object.keys(rpsHashy);
-let playaPlay = window.prompt("Pick one: rock, paper, scissors");
-const computuhPlay = keys[Math.floor((Math.random() * 3))];
-
-function play(computuhPlay, playaPlay) {
-    if (computuhPlay === playaPlay)
-        return console.log("It's a tie!");
-    else if (computuhPlay === rpsHashy[playaPlay][0])
-        return console.log("You win!");
-    else
-        return console.log("Computer wins!");
+function computuhPlays() {
+    let keys = Object.keys(rpsHashy);
+    return keys[Math.floor((Math.random() * 3))];
 }
-play(computuhPlay, playaPlay);
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+function play(playaPlay) {
+    let computuhPlay = computuhPlays();
+    let result = "";
 
+    if (computuhPlay == rpsHashy[playaPlay][0]) {
+        playaScore += 1;
+        result = ('You win! ' + playaPlay + ' beats ' + computuhPlay
+            + "<br><br>Player score: " + playaScore + "<br>Computer score: " + computuhScore);
+        if (playaScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again';
+            disableButtons();
+        }
+    } else if (computuhPlay == playaPlay) {
+        result = ('It\'s a tie. You both chose ' + playaPlay + "<br><br>Player score: " + playaScore + "<br>Computer score: " + computuhScore)
+    } else {
+        computuhScore += 1
+        result = ('You lose! ' + computuhPlay+ ' beats ' + playaPlay
+        + "<br><br>Player score: " + playaScore + "<br>Computer score: " + computuhScore)
+
+    if (computuhScore == 5) {
+        result += '<br><br>I won the game! Reload the page to play again'
+        disableButtons();
+    }
+}
+    document.getElementById('result').innerHTML = result
+    return
+}
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            play(button.value)
+    })
+})
